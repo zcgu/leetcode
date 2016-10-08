8da9825ded395bc35191a6927cdd378a71734369
zcgu <zcgu@users.noreply.github.com>
2016-10-08 15:02:42 -0500

    Update 231. Power of Two.py

--- codes/231. Power of Two.py
+++ codes/231. Power of Two.py
@@ -4,8 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        # (n - 1) & n == 0 判断2的pow，也就是只有1个1的方式
+        
         if n <= 0: return False
-        return (2 ** 50) % n == 0
+        # return (2 ** 50) % n == 0
+        return (n - 1) & n == 0
         
         # if n == 0:
         #     return False
