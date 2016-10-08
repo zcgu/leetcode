d1f06528816f28a074de2e4a13d361b8139baba3
zcgu <zcgu@users.noreply.github.com>
2016-10-08 14:06:59 -0500

    Create 371. Sum of Two Integers.java

---
+++ codes/371. Sum of Two Integers.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int tmp = a;
+            a = a ^ b;
+            b = (tmp & b) << 1;
+        }
+        
+        return a;
+    }
+}
