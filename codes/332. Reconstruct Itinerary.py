bff3e265c3c0d706075faec9f2c1642e5ee36c21
zcgu <zcgu@users.noreply.github.com>
2016-10-08 16:39:20 -0500

    Update 332. Reconstruct Itinerary.py

--- codes/332. Reconstruct Itinerary.py
+++ codes/332. Reconstruct Itinerary.py
@@ -4,45 +4,17 @@ class Solution(object):
         :type tickets: List[List[str]]
         :rtype: List[str]
         """
+        tickets.sort()
+        return self.dfs(['JFK'], tickets)
+        
+        
+    def dfs(self, lst, tickets):
         if not tickets:
-            return []
+            return lst
             
-        self.n = len(tickets) + 1
-        
-        table = {}
-        for f, e in tickets:
-            table[f] = table[f] + [e] if f in table else [e]
-        
-        for vals in table.values():
-            vals.sort()
-        
-        self.res = []
-        
-        self.dfs(['JFK'], table)
-        
-        return self.res
-        
-        
-    def dfs(self, lst, table):
-        # print lst
-        if len(lst) == self.n:
-            self.res = lst
-            return True
-        
-        if lst[-1] not in table or not table[lst[-1]]:
-            # print 1, table, lst[-1]
-            return False
-        
-        for i in range(len(table[lst[-1]])):
-            # print table
-            lst.append(table[lst[-1]][i])
-            del table[lst[-2]][i]
-            if self.dfs(lst, table):
-                return True
-            table[lst[-2]].insert(i, lst[-1])
-            del lst[-1]
-        
-        
-        return False
-        
-        
+        for i, t in enumerate(tickets):
+            if t[0] == lst[-1]:
+                res = self.dfs(lst + [t[1]], tickets[:i] + tickets[i+1:])
+                if res:
+                    return res
+        return None
