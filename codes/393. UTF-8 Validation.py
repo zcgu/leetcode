83c2e39afd653c221473c43a2617a3e0ce04705b
zcgu <zcgu@users.noreply.github.com>
2016-10-08 13:36:36 -0500

    Update 393. UTF-8 Validation.py

--- codes/393. UTF-8 Validation.py
+++ codes/393. UTF-8 Validation.py
@@ -4,51 +4,31 @@ class Solution(object):
         :type data: List[int]
         :rtype: bool
         """
-        lst = []
+        # 这题挺烦
+        # 从左往右走，
+        # 1.第一位是0，count必须是0. 
+        # 2.第一位是1，count大于0，第二位必须是0 
+        # 3.count为0，数开头的1，必须大于1个
+        # 4.最后count必须是0
         
-        for d in data:
-            lst.append(self.convert(d))
+        if not data: return False
         
-        while lst:
-            if lst[0][0] == '0':
-                del lst[0]
-                continue
-            if lst[0][0] == '1' and lst[0][1] == '0':
-                
-                return False
-            
-            count = 0
-            start = -1
-            for i, s in enumerate(lst):
-                if start != -1:
-                    break
-                for c in s:
-                    if c == '1':
-                        count += 1
-                    else:
-                        start = i
-                        break
-    
-            for i in range(start + 1, start + count):
-                if i >= len(lst):
-                    
+        count = 0
+        for p in range(len(data)):
+            if data[p] & 0x80 == 0:
+                if count != 0:
                     return False
-                if lst[i][0] != '1' or lst[i][1] != '0':
-                    
+            elif count != 0:
+                if data[p] & 0x40 != 0:
                     return False
-            
-            for _ in range(count):
-                del lst[0]
-            
-        return True
+                count -= 1
+            else:
+                i = 6
+                while i > 0 and data[p] & ( 1 << i) != 0:
+                    count += 1
+                    i -= 1
+                if count == 0:
+                    return False
         
-        
-    def convert(self, num):
-        res = ''
-        
-        for _ in range(8):
-            res = str(num % 2) + res
-            num /= 2
-        
-        return res
+        return count == 0
         
