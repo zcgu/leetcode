4e1243cf323fa80be6f12947af7f634995dd9897
zcgu <zcgu@users.noreply.github.com>
2016-10-08 11:48:55 -0500

    Update 115. Distinct Subsequences.py

--- codes/115. Distinct Subsequences.py
+++ codes/115. Distinct Subsequences.py
@@ -5,19 +5,18 @@ class Solution(object):
         :type t: str
         :rtype: int
         """
-        # 类似edit distance，如果s[i] t[i]相等可以加上dp[i-1][j-1], 再加上dp[i][j-1]。
-        dp = [[0 for _ in range(len(s) + 1)] for _ in range(len(t) + 1)]
+        dp = [[0 for _ in range(len(t) + 1)] for _ in range(len(s) + 1)]
         
-        for j in range(len(s) + 1):
-            dp[0][j] = 1
+        for i in range(len(s) + 1):
+            dp[i][0] = 1
         
-        for i in range(1, len(t) + 1):
-            for j in range(1, len(s) + 1):
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(t) + 1):
                 dp[i][j] = 0
                 
-                if t[i - 1] == s[j -1]:
+                if s[i - 1] == t[j -1]:
                     dp[i][j] += dp[i - 1][j - 1]
                 
-                dp[i][j] += dp[i][j - 1]
+                dp[i][j] += dp[i - 1][j]
         
         return dp[-1][-1]
