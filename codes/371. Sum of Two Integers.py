0e3ae97f0430a8df24420391e0f41ad823acbecf
zcgu <zcgu@users.noreply.github.com>
2016-10-08 14:03:07 -0500

    Update 371. Sum of Two Integers.py

--- codes/371. Sum of Two Integers.py
+++ codes/371. Sum of Two Integers.py
@@ -5,6 +5,17 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        
-        list = [a, b]
-        return sum(list)
+#       bit 操作在python太麻烦了，还是用java吧。。。
+
+        # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # 32 bits interger min
+        MIN = 0x80000000
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
+        while b != 0:
+            # ^ get different bits and & gets double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
+        return a if a <= MAX else ~(a ^ mask)
