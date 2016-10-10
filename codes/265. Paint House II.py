9af4501170733793408016b380c755cf42f6b80c
zcgu <zcgu@users.noreply.github.com>
2016-10-10 18:43:59 -0500

    Create 265. Paint House II.py

---
+++ codes/265. Paint House II.py
@@ -0,0 +1,30 @@
+# There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+# The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
+
+# Note:
+# All costs are positive integers.
+
+# Follow up:
+# Could you solve it in O(nk) runtime?
+
+class Solution(object):
+    def minCostII(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        
+        if not costs: return 0
+        if len(costs[0]) == 1: return costs[0][0]
+        
+        tmp = [0 for _ in range(len(costs[0]))]
+        
+        for cost in costs:
+            
+            tmp2 = tmp[:]
+            for i in range(len(costs[0])):
+                tmp2[i] = cost[i] + min(tmp[:i] + tmp[i+1:])
+            tmp = tmp2[:]
+        
+        return min(tmp)
