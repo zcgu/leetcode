d0a3a58d4b5b3a40f1c38302323aa310222bdaed
zcgu <zcgu@users.noreply.github.com>
2016-10-10 02:29:11 -0500

    Update 77. Combinations.py

--- codes/77. Combinations.py
+++ codes/77. Combinations.py
@@ -5,27 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        return self.dfs(n, k)
+    
+    def dfs(self, n, k):
+        if k == 0: return [[]]
+        if n == 0 or n < k: return []   # 加了一个小小的检查，就过了
         
-        res = [[]]
+        res = []
+        for lst in self.dfs(n - 1, k):
+            res.append(lst)
+        for lst in self.dfs(n - 1, k - 1):
+            res.append([n] + lst)
         
-        for i in range(1, n + 1):
-            for j in range(len(res)):
-                if len(res[j]) < k:
-                    res.append(res[j] + [i])
+        return res
+        
+        # res = [[]]
+        
+        # for i in range(1, n + 1):
+        #     for j in range(len(res)):
+        #         if len(res[j]) < k:
+        #             res.append(res[j] + [i])
             
             
-        return [lst for lst in res if len(lst) == k]
+        # return [lst for lst in res if len(lst) == k]
         
         
-                
-    #     self.res = []
-    #     self.recur(n, k, 1, [])
-    #     return self.res
-        
-    # def recur(self, n, k, num, lst):
-    #     if len(lst) == k:
-    #         self.res.append(lst)
-    #         return
-        
-    #     for i in range(num, n+1):
-    #         self.recur(n, k, i+1, lst + [i])
