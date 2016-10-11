2c431da7899ebcc1af1ac8e232c0c64ede110204
zcgu <zcgu@users.noreply.github.com>
2016-10-10 23:52:24 -0500

    Update 189. Rotate Array.py

--- codes/189. Rotate Array.py
+++ codes/189. Rotate Array.py
@@ -5,17 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        k = k % len(nums)
         
-        k %= len(nums)
+        mid = len(nums) - k
         
-        tmp = []
+        p1 = 0
+        p2 = mid - 1
+        while p1 < p2:
+            nums[p1], nums[p2] = nums[p2], nums[p1]
+            p1 += 1
+            p2 -= 1
         
-        for i in range(len(nums) -k, len(nums)):
-            tmp.append(nums[i])
+        p1 = mid
+        p2 = len(nums) - 1
+        while p1 < p2:
+            nums[p1], nums[p2] = nums[p2], nums[p1]
+            p1 += 1
+            p2 -= 1
         
-        for i in range(len(nums) - 1, k - 1, -1):
-            nums[i] = nums[i - k]
-        
-        for i in range(k):
-            nums[i] = tmp[i]
+        p1 = 0
+        p2 = len(nums) - 1
+        while p1 < p2:
+            nums[p1], nums[p2] = nums[p2], nums[p1]
+            p1 += 1
+            p2 -= 1
         
