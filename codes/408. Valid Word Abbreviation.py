ab49fb22f9be013e603892652fd611b222a6e183
zcgu <zcgu@users.noreply.github.com>
2016-10-15 00:52:31 -0500

    Create 408. Valid Word Abbreviation.py

---
+++ codes/408. Valid Word Abbreviation.py
@@ -0,0 +1,65 @@
+# Given a non-empty string s and an abbreviation abbr, return whether the string matches with the given abbreviation.
+
+# A string such as "word" contains only the following valid abbreviations:
+
+# ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+# Notice that only the above abbreviations are valid abbreviations of the string "word". Any other string is not a valid abbreviation of "word".
+
+# Note:
+# Assume s contains only lowercase letters and abbr contains only lowercase letters and digits.
+
+# Example 1:
+# Given s = "internationalization", abbr = "i12iz4n":
+
+# Return true.
+# Example 2:
+# Given s = "apple", abbr = "a2e":
+
+# Return false.
+# Show Company Tags
+# Show Tags
+# Show Similar Problems
+
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+        if word != '' and abbr is None:
+            return False
+        
+        if word == '' and abbr == '':
+            return True
+        
+        if word == '' and abbr == '0':
+            return True
+        
+        
+        p1 = 0
+        p2 = 0
+        
+        while p1 < len(word) and p2 < len(abbr):
+            if abbr[p2].isdigit():
+                if abbr[p2] == '0':
+                    return False
+                    
+                start = p2
+                while p2 < len(abbr) and abbr[p2].isdigit():
+                    p2 += 1
+                num = int(abbr[start: p2])
+                
+                p1 += num
+                    
+            else:
+                if word[p1] != abbr[p2]:
+                    return False
+                else:
+                    p1 += 1
+                    p2 += 1
+        
+        if p1 == len(word) and p2 == len(abbr):
+            return True
+        else:
+            return False
