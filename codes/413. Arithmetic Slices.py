f383824f48f70daf3b8145f999e55be5c5ef3a5d
zcgu <zcgu@users.noreply.github.com>
2016-10-15 01:22:12 -0500

    Create 413. Arithmetic Slices.py

---
+++ codes/413. Arithmetic Slices.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        dp = [0 for _ in range(len(A))]
+        
+        for i in range(2, len(A)):
+            if A[i] - A[i-1] == A[i-1] - A[i-2]:
+                dp[i] = dp[i - 1] + 1
+        
+        return sum(dp)
