059e9a824c4125982e5198c59367749f3473d5c0
zcgu <zcgu@users.noreply.github.com>
2016-10-17 19:10:58 -0500

    Update sort.py

--- sort.py
+++ sort.py
@@ -20,3 +20,29 @@
         nums[p1] = mid
         self.qsort(nums, a, p1-1)
         self.qsort(nums, p1 + 1, b)
+
+        
+#   2. merge sort
+
+    def mergeSort(self, nums):
+        if len(nums) <= 1: return nums
+        
+        nums1 = self.mergeSort(nums[:len(nums) / 2])
+        nums2 = self.mergeSort(nums[len(nums) / 2:])
+        
+        res = []
+        
+        p1 = p2 = 0
+        
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] <= nums2[p2]:
+                res.append(nums1[p1])
+                p1 += 1
+            else:
+                res.append(nums2[p2])
+                p2 += 1
+        
+        res += nums1[p1:]
+        res += nums2[p2:]
+        
+        return res
