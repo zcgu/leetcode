ac010daa1a95ca8d660da4ca442b12d43f04f8fd
zcgu <zcgu@users.noreply.github.com>
2016-10-17 20:31:55 -0500

    Update 13. Roman to Integer.py

--- codes/13. Roman to Integer.py
+++ codes/13. Roman to Integer.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # I, II, III, IV, V, VI, VII, VIII, IX, X. 从右往左变大，变小的那个要减去
+        
         table = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         
         res = 0
