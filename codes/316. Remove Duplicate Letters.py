e97cd4428937b337a51745edbf9131c5fa343205
zcgu <zcgu@users.noreply.github.com>
2016-10-30 14:41:05 -0500

    Update 316. Remove Duplicate Letters.py

--- codes/316. Remove Duplicate Letters.py
+++ codes/316. Remove Duplicate Letters.py
@@ -4,26 +4,59 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        table = {chr(ord('a') + i): s.count(chr(ord('a') + i)) for i in range(26)}
-
-        visit = [False for _ in range(26)]
+        table = {}
+        for c in s:
+            table[c] = table.get(c, 0) + 1
         
         stack = []
         
         for c in s:
-            table[c] -= 1
-            
-            if visit[ord(c) - ord('a')]:
+            if c in stack:
+                table[c] -= 1
                 continue
             
-            while stack and stack[-1] > c and table[stack[-1]] > 0:
-                visit[ord(stack[-1]) - ord('a')] = False
-                stack.pop()
+            if not stack or c > stack[-1]:
+                stack.append(c)
+                table[c] -= 1
+            else:
+                while stack and c <= stack[-1]:
+                    if table[stack[-1]] > 0:
+                        c2 = stack.pop()
+                    else:
+                        break
+                stack.append(c)
+                table[c] -= 1
                 
-            visit[ord(c) - ord('a')] = True
-            stack.append(c)
-        
         return ''.join(stack)
+
+
+
+# class Solution(object):
+#     def removeDuplicateLetters(self, s):
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
+#         table = {chr(ord('a') + i): s.count(chr(ord('a') + i)) for i in range(26)}
+
+#         visit = [False for _ in range(26)]
+        
+#         stack = []
+        
+#         for c in s:
+#             table[c] -= 1
+            
+#             if visit[ord(c) - ord('a')]:
+#                 continue
+            
+#             while stack and stack[-1] > c and table[stack[-1]] > 0:
+#                 visit[ord(stack[-1]) - ord('a')] = False
+#                 stack.pop()
+                
+#             visit[ord(c) - ord('a')] = True
+#             stack.append(c)
+        
+#         return ''.join(stack)
         
         
         
