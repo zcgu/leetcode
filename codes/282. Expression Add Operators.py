d8a0073627a9dc52c94d9a76696cdd9432d49ad6
zcgu <zcgu@users.noreply.github.com>
2016-10-29 22:13:04 -0500

    Update 282. Expression Add Operators.py

--- codes/282. Expression Add Operators.py
+++ codes/282. Expression Add Operators.py
@@ -5,40 +5,31 @@ class Solution(object):
         :type target: int
         :rtype: List[str]
         """
-        # 这个题烦
+        return self.dfs(num, target, 1)
         
-        if not num: return []
-        
-        return self.dfs(num, target, 1, False)
-        
-    def dfs(self, num, target, m, isMulti):
-        if not num:
-            if isMulti and target == m:
-                return ['']
-            elif not isMulti and target == 0:
-                return ['']
-            else:
-                return []
-        
+    def dfs(self, num, target, m):
+
         res = []
-        for i in range(len(num) - 1, -1, -1):   # 必须从后往前，因为‘－’后面的括起来要变号。
-            a = num[i:]
-            if len(a) > 1 and a[0] == '0':
+        for i in range(len(num) - 1, -1, -1):       # 必须从后往前，因为‘－’后面的括起来要变号。
+            first, second = num[:i], num[i:]
+            
+            if len(second) > 1 and second[0] == '0': continue
+            
+            if first == '':                     # 特殊情况在这处理了，一般在开头和下面的循环用if处理。
+                if int(second) * m == target:
+                    res.append(second)
                 continue
-            a = int(a)
             
-            if i == 0:  # 特殊情况在这处理了，一般在下面的循环用if s：处理。这个题第三个是True，就不行了。
-                for s in self.dfs(num[:i], target - a * m, 1, False):
-                    res.append(num[i:])
-                break
-                
-            for s in self.dfs(num[:i], target - a * m, 1, False):
-                res.append(s + '+' + num[i:])
+            # +
+            for s in self.dfs(first, target - int(second) * m, 1):
+                res.append(s + '+' + second)
             
-            for s in self.dfs(num[:i], a * m + target, 1, False):
-                res.append(s + '-' + num[i:])
-                
-            for s in self.dfs(num[:i], target, a * m, True):
-                res.append(s + '*' + num[i:])
+            # -
+            for s in self.dfs(first, target + int(second) * m, 1):
+                res.append(s + '-' + second)
             
+            # *
+            for s in self.dfs(first, target, m * int(second)):
+                res.append(s + '*' + second)
+        
         return res
